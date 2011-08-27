b0f84c319bb60d6d512ddc206bc329a3e4b9c82a
Dominik Liebler <dominik@liebler-web.de>
2011-08-27 10:05:15 +0200

    added new example for Composite pattern

--- Composite/Composite.php
+++ Composite/Composite.php
@@ -11,6 +11,7 @@ namespace DesignPatterns;
  * Example:
  * - a form class instance handles all its form elements like a single instance of the form, when render() is called, it
  *      subsequently runs trough all its child elements and calls render() on them
+ * - Zend_Config: a tree of configuration options, each one is a Zend_Config object
  *
  */
 class Form
