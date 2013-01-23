1d554ddce3b79159253e84e7f3fa4e37cc8a54ef
Dominik Liebler <liebler.dominik@googlemail.com>
2013-01-23 13:16:36 +0100

    Update DependenyInjection/DependencyInjection.php

--- DependenyInjection/DependencyInjection.php
+++ DependenyInjection/DependencyInjection.php
@@ -6,14 +6,14 @@ namespace DesignPatterns;
  * Dependency Injection
  *
  * Purpose:
- * to implement a loosely coupled architecture in order to get better testable, maintainable and extandable code
+ * to implement a loosely coupled architecture in order to get better testable, maintainable and extendable code
  * 
  * Examples:
  * - the Doctrine2 ORM uses dependency injection e.g. for Configuration that is injected into a Connection object. for
  *   testing purposes, one can easily create a mock object of the configuration and inject that into the connection
  *   object
  * - Symfony and Zend Framework 2 already have containers for DI that create objects via a configuration array and
- *   inject them where needed
+ *   inject them where needed (i.e. in Controllers)
  *
  */
 class Configuration
