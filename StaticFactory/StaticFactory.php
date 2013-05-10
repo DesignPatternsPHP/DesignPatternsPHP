3a3937f339daca05b907461b98cd28099fcd7685
Trismegiste <florent.g@allopneus.com>
2013-05-10 21:49:22 +0200

    typo

--- StaticFactory/StaticFactory.php
+++ StaticFactory/StaticFactory.php
@@ -7,7 +7,7 @@ namespace DesignPatterns\StaticFactory;
  *
  * Purpose:
  * similar to the AbstractFactory, this pattern is used to create series of related or dependant objects.
- * The difference between this and the abstract factory pattern is that the factory method pattern uses just one static
+ * The difference between this and the abstract factory pattern is that the static factory pattern uses just one static
  * method to create all types of objects it can create. It is usually named "factory" or "build".
  *
  * Examples:
