9278f295b009f562e185da5eef89a80508953eeb
Trismegiste <florent.g@allopneus.com>
2013-05-11 20:54:21 +0200

    more real example

---
+++ Adapter/Kindle.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ * DesignPatternPHP
+ */
+
+namespace DesignPatterns\Adapter;
+
+/**
+ * Kindle is a concrete electronic book
+ *
+ * @author flo
+ */
+class Kindle implements ElecBookInterface
+{
+
+    public function pressNext()
+    {
+        
+    }
+
+    public function pressStart()
+    {
+        
+    }
+
+}
