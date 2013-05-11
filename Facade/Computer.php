22dbd54cbb1132756a30123e10fb359d01dc448d
Trismegiste <florent.g@allopneus.com>
2013-05-11 16:20:55 +0200

    comments

--- Facade/Computer.php
+++ Facade/Computer.php
@@ -12,17 +12,19 @@ namespace DesignPatterns\Facade;
  * 
  * The first goal is to reduce coupling and folow the Law of Demeter.
  * 
- * A Facade is meant to decouple a client and a sub-system with
+ * A Facade is meant to decouple a client and a sub-system by embedding
  * many (but sometimes just one) interface, and of course to reduce complexity.
  * 
  * 1. A facade does not forbid you the access to the sub-system
- * 2. You can (you shoud) have multiple facades for one sub-system
+ * 2. You can (you should) have multiple facades for one sub-system
  * 
  * That's why a good facade has no "new" in it. If there are multiple creations
  * for each method, it is not a Facade, it's a Builder or a 
- * [Abstract|Static|Simple] Factory [Method]
+ * [Abstract|Static|Simple] Factory [Method]. 
  *
- * The best facade has no new and a constructor with interface-type-hinted parameters
+ * The best facade has no new and a constructor with interface-type-hinted parameters.
+ * If you need creation of new instances, use Factory as argument. 
+ * 
  */
 class Computer
 {
@@ -30,6 +32,10 @@ class Computer
     protected $opsys;
     protected $bios;
 
+    /**
+     * This is the perfect time to use a dependency injection container
+     * to creaate an instance of this class
+     */
     public function __construct(BiosInterface $bios, OsInterface $os)
     {
         $this->bios = $bios;
