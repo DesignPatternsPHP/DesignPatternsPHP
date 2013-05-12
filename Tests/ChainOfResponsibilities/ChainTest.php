71a43e2e93e79096255f9ad336c39f120c194a39
Trismegiste <florent.g@allopneus.com>
2013-05-12 22:08:56 +0200

    tests for CoR

--- Tests/ChainOfResponsibilities/ChainTest.php
+++ Tests/ChainOfResponsibilities/ChainTest.php
@@ -7,6 +7,7 @@
 namespace DesignPatterns\Tests\ChainOfResponsibilities;
 
 use DesignPatterns\ChainOfResponsibilities\Request;
+use DesignPatterns\ChainOfResponsibilities\Responsible;
 
 /**
  * ChainTest tests the CoR
@@ -14,9 +15,22 @@ use DesignPatterns\ChainOfResponsibilities\Request;
 class ChainTest extends \PHPUnit_Framework_TestCase
 {
 
+    protected $chain;
+
+    protected function setUp()
+    {
+        $this->chain = new Responsible\FastStorage(array('bar' => 'baz'));
+        $this->chain->append(new Responsible\SlowStorage(array('bar' => 'baz', 'foo' => 'bar')));
+    }
+
     public function testProcess()
     {
-        
+        $request = new Request();
+        $request->verb = 'get';
+        $request->key = 'bar';
+
+        $ret = $this->chain->handle($request);
+        print_r($request);
     }
 
 }
