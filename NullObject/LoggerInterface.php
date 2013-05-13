f157729ea42c0f3188b53b16ef7499ebf038c45a
Trismegiste <florent.g@allopneus.com>
2013-05-13 21:57:52 +0200

    add comments about null logger

--- NullObject/LoggerInterface.php
+++ NullObject/LoggerInterface.php
@@ -8,6 +8,8 @@ namespace DesignPatterns\NullObject;
 
 /**
  * LoggerInterface is a contract for logging something
+ * 
+ * Key-feaature : NullLogger MUST inherit from this interface like any other Loggers
  */
 interface LoggerInterface
 {
