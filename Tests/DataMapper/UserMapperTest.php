51fe7d477f31b48ff9f101324f6708a0affc4a14
Trismegiste <florent.g@allopneus.com>
2013-07-08 04:25:19 +0200

    making a unit test class with mockup instead of unfinished script

---
+++ Tests/DataMapper/UserMapperTest.php
@@ -0,0 +1,61 @@
+<?php
+
+/*
+ * DesignPatternPHP
+ */
+
+namespace DesignPatterns\Test\DataMapper;
+
+use DesignPatterns\DataMapper\UserMapper;
+use DesignPatterns\DataMapper\User;
+
+/**
+ * UserMapperTest tests the datamappe pattern
+ */
+class UserMapperTest extends \PHPUnit_Framework_TestCase
+{
+
+    protected $mapper;
+    protected $dbal;
+
+    protected function setUp()
+    {
+        $this->dbal = $this->getMockBuilder('DesignPatterns\DataMapper\DBAL')
+                ->disableAutoload()
+                ->setMethods(array('insert', 'update', 'find', 'findAll'))
+                ->getMock();
+
+        $this->mapper = new UserMapper($this->dbal);
+    }
+
+    public function getNewUser()
+    {
+        return array(array(new User(null, 'Odysseus', 'Odysseus@ithaca.gr')));
+    }
+
+    public function getExistingUser()
+    {
+        return array(array(new User(1, 'Odysseus', 'Odysseus@ithaca.gr')));
+    }
+
+    /**
+     * @dataProvider getNewUser
+     */
+    public function testPersistNew(User $user)
+    {
+        $this->dbal->expects($this->once())
+                ->method('insert');
+        $this->mapper->save($user);
+    }
+
+    /**
+     * @dataProvider getExistingUser
+     */
+    public function testPersistExisting(User $user)
+    {
+        $this->dbal->expects($this->once())
+                ->method('update');
+        $this->mapper->save($user);
+    }
+
+}
