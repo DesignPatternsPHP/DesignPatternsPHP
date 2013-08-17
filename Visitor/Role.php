67bdb9fede894be955bd39ab9c69e62434b09ea7
Trismegiste <flo@trismegiste.net>
2013-08-17 19:41:18 -0400

    documentation (commiting now from Guadeloupe)

--- Visitor/Role.php
+++ Visitor/Role.php
@@ -9,13 +9,14 @@ namespace DesignPatterns\Visitor;
  * The Visitor Pattern lets you outsource operations on objects to other objects. The main reason to do this is to keep
  * a seperation of concerns. But classes have to define an contract to allow visitors (the "accept" method in the example below).
  * 
- * The contract is an abstract class but you can have also a clean interface
+ * The contract is an abstract class but you can have also a clean interface. 
+ * In that case, each Visitee has to choose itself which method to invoke on the visitor.
  */
 abstract class Role
 {
 
     /**
-     * This method handle a double dispatch based on the shortname of the Visitee
+     * This method handles a double dispatch based on the shortname of the Visitee
      * 
      * Feel free to override it if your object must call another visiting behavior
      * 
--- Visitor/RolePrintVisitor.php
+++ Visitor/RolePrintVisitor.php
@@ -5,16 +5,22 @@ namespace DesignPatterns\Visitor;
 /**
  * Visitor Pattern
  *
- * An implementation of a Visitor
+ * An implementation of a concrete Visitor
  */
 class RolePrintVisitor implements RoleVisitor
 {
 
+    /**
+     * @inheritdoc
+     */
     public function visitGroup(Group $role)
     {
         echo "Role: " . $role->getName();
     }
 
+    /**
+     * @inheritdoc
+     */
     public function visitUser(User $role)
     {
         echo "Role: " . $role->getName();
