52ef48fd28ca40ed9e4e48dd05955c441280b279
Oleksandr <olekhy@googlemail.com>
2013-09-21 22:05:13 +0200

    Update Singleton.php
    Hi! I'm sure current realization of singleton is classic variance of the pattern. But not really correct (more then is mistaken for me) in relation to real codding on PHP projects. ;)
    
    ```php
    class A extends Singleton // Singleton which used static inside of getInstance() method  
    {}
    
    class B extends A
    {}
    
    $a = A::getInstance();
    $b = B::getInstance();
    $c = Singleton::getInstance();
    
    $a === $b && $b === $c; // returned FALSE
    ```
    when u would be utilized Singleton updated then expression above returned TRUE.

--- Singleton/Singleton.php
+++ Singleton/Singleton.php
@@ -17,19 +17,23 @@ namespace DesignPatterns;
 class Singleton
 {
     /**
+     * @var cached reference to singleton instance 
+     */
+    protected static $instance;
+    
+    /**
      * gets the instance via lazy initialization (created on first usage)
      *
      * @return Singleton
      */
     public static function getInstance()
     {
-        static $instance;
-
-        if (null === $instance) {
-            $instance = new self();
+        
+        if (null === static::$instance) {
+            static::$instance = new static;
         }
 
-        return $instance;
+        return static::$instance;
     }
 
     /**
