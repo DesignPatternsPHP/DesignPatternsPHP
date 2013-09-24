f8e801d445dcc8ef25a5de540891383c8b6bfaf1
Dominik Liebler <liebler.dominik@googlemail.com>
2013-09-24 14:18:33 +0200

    README AbstractFactory

--- AbstractFactory/AbstractFactory.php
+++ AbstractFactory/AbstractFactory.php
@@ -3,22 +3,17 @@
 namespace DesignPatterns\AbstractFactory;
 
 /**
- * Abstract Factory pattern
+ * class AbstractFactory
+ * 
+ * Sometimes also known as "Kit" in a GUI libraries.
  *
- * Purpose:
- * to create series of related or dependant objects without specifying their concrete classes,
- * usually the created classes all implement the same interface. The client of the abstract
- * factory does not care about how these objects are created, he just knows they go together.
- * 
- * Sometimes also known as "Kit" in a GUI libraries. 
- * 
  * This design pattern implements the Dependency Inversion Principle since
  * it is the concrete subclass which creates concrete components.
- * 
+ *
  * In this case, the abstract factory is a contract for creating some components
- * for the web. There are two components : Text and Picture. There is two ways 
+ * for the web. There are two components : Text and Picture. There is two ways
  * of rendering : HTML or JSON.
- * 
+ *
  * Therefore 4 concretes classes, but the client just need to know this contract
  * to build a correct http response (for a html page or for an ajax request)
  */
---
+++ AbstractFactory/README.md
@@ -0,0 +1,6 @@
+# Abstract Factory
+
+## Purpose
+
+To create series of related or dependant objects without specifying their concrete classes.
+Usually the created classes all implement the same interface. The client of the abstract factory does not care about how these objects are created, he just knows how they go together.
