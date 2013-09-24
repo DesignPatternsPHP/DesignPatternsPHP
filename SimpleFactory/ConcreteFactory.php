d05a8beb9c436d5f2946c6c920267cad06d2b5aa
Dominik Liebler <liebler.dominik@googlemail.com>
2013-09-24 14:08:25 +0200

    README SimpleFactory

--- SimpleFactory/ConcreteFactory.php
+++ SimpleFactory/ConcreteFactory.php
@@ -3,13 +3,7 @@
 namespace DesignPatterns\SimpleFactory;
 
 /**
- * ConcreteFactory is a simple factory pattern.
- * 
- * It differs from the static factory because it is NOT static and as you
- * know : static => global => evil
- * 
- * Therefore, you can haZ multiple factories, differently parametrized, 
- * you can subclass it and you can mock-up it.
+ * class ConcreteFactory
  */
 class ConcreteFactory
 {
---
+++ SimpleFactory/README.md
@@ -0,0 +1,9 @@
+# Simple Factory
+
+## Purpose
+
+ConcreteFactory is a simple factory pattern.
+
+It differs from the static factory because it is NOT static and as you know: static => global => evil!
+
+Therefore, you can haZ multiple factories, differently parametrized, you can subclass it and you can mock-up it.
