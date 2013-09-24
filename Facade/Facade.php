d618d2068af17dfc6e9b9445187bece3c3790e08
Dominik Liebler <liebler.dominik@googlemail.com>
2013-09-24 13:29:38 +0200

    README Facade

--- Facade/Facade.php
+++ Facade/Facade.php
@@ -3,23 +3,7 @@
 namespace DesignPatterns\Facade;
 
 /**
- * The primary goal of a Facade Pattern is not to avoid you to read the manual of
- * a complex API. It's only a side-effect.
- * 
- * The first goal is to reduce coupling and follow the Law of Demeter.
- * 
- * A Facade is meant to decouple a client and a sub-system by embedding
- * many (but sometimes just one) interface, and of course to reduce complexity.
- * 
- * 1. A facade does not forbid you the access to the sub-system
- * 2. You can (you should) have multiple facades for one sub-system
- * 
- * That's why a good facade has no "new" in it. If there are multiple creations
- * for each method, it is not a Facade, it's a Builder or a 
- * [Abstract|Static|Simple] Factory [Method]. 
  *
- * The best facade has no new and a constructor with interface-type-hinted parameters.
- * If you need creation of new instances, use Factory as argument. 
  * 
  */
 class Facade
---
+++ Facade/README.md
@@ -0,0 +1,17 @@
+# Facade
+
+## Purpose
+
+The primary goal of a Facade Pattern is not to avoid you to read the manual of a complex API. It's only a side-effect.
+The first goal is to reduce coupling and follow the Law of Demeter.
+
+A Facade is meant to decouple a client and a sub-system by embedding many (but sometimes just one) interface, and of course to reduce complexity.
+
+* A facade does not forbid you the access to the sub-system
+* You can (you should) have multiple facades for one sub-system
+
+That's why a good facade has no `new` in it. If there are multiple creations for each method, it is not a Facade, it's a Builder or a
+[Abstract|Static|Simple] Factory [Method].
+
+The best facade has no `new` and a constructor with interface-type-hinted parameters.
+If you need creation of new instances, use a Factory as argument.
