5633c8508cae66dd06981c077730242a869666a1
Dominik Liebler <liebler.dominik@googlemail.com>
2013-09-24 13:44:11 +0200

    README Observer

--- Observer/UserObserver.php
+++ Observer/UserObserver.php
@@ -3,17 +3,7 @@
 namespace DesignPatterns\Observer;
 
 /**
- * Observer pattern
- *
- * Purpose:
- * to implement a publish/subscribe behaviour to an object, whenever a "Subject" object changes it's state, the attached
- * "Observers" will be notified. It is used to shorten the amount of coupled objects and uses loose coupling instead
- *
- * Examples:
- * - a message queue system is observed to show the progress of a job in a GUI
- *
- * PHP already defines two interfaces that can help to implement this pattern: SplObserver and SplSubject
- *
+ * class UserObserver
  */
 class UserObserver implements \SplObserver
 {
