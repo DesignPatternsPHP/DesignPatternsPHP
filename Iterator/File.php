0251304b9a613372710f99dea7dd8a168b934a3c
Dominik Liebler <liebler.dominik@googlemail.com>
2013-09-24 13:35:11 +0200

    README iterator

--- Iterator/File.php
+++ Iterator/File.php
@@ -3,21 +3,9 @@
 namespace DesignPatterns\Iterator;
 
 /**
- * iterator pattern
- *
- * Purpose:
- * to make an object iterable
- *
- * Examples:
- * - to process a file line by line by just running over all lines (which have an object representation) for a file
- *   (which of course is an object, too)
- *
- * Note:
- * Standard PHP Library (SPL) defines an interface Iterator which is best suited for this!
- * Often you would want to implement the Countable interface too, to allow count($object) on your iterable object
+ * class File
  *
  * THIS EXAMPLE ALSO APPLIES THE COMPOSITE PATTERN
- *
  */
 class File
 {
