6edac3f229fb722a779b0e688056ca99904cda1f
martin <mbuckley@gmail.com>
2013-11-24 22:30:06 +0000

    Added Specification pattern

---
+++ Specification/Plus.php
@@ -0,0 +1,37 @@
+<?php
+namespace DesignPatterns\Specification;
+
+/**
+ * A logical AND specification
+ */
+class Plus extends AbstractSpecification
+{
+
+    protected $left;
+    protected $right;
+
+    /**
+     * Creation of a locical AND of two specifications
+     * 
+     * @param SpecificationInterface $left
+     * @param SpecificationInterface $right
+
+     */
+    public function __construct(SpecificationInterface $left, SpecificationInterface $right)
+    {
+        $this->left = $left;
+        $this->right = $right;
+    }
+
+    /**
+     * Checks if the composite AND of specifications passes
+     * 
+     * @param Item $item
+     * 
+     * @return bool
+     */
+    public function isSatisfiedBy(Item $item)
+    {
+        return $this->left->isSatisfiedBy($item) && $this->right->isSatisfiedBy($item);
+    }
+}
---
+++ Specification/PriceSpecification.php
@@ -0,0 +1,50 @@
+<?php
+namespace DesignPatterns\Specification;
+
+/**
+ * A specification to check an Item is priced between min and max
+ */
+class PriceSpecification extends AbstractSpecification
+{
+    protected $maxPrice;
+    protected $minPrice;
+
+    /**
+     * Sets the optional maximum price
+     *
+     * @param int $maxPrice
+     */
+    public function setMaxPrice($maxPrice)
+    {
+        $this->maxPrice = $maxPrice;
+    }
+
+    /**
+     * Sets the optional minimum price
+     *
+     * @param int $minPrice
+     */
+    public function setMinPrice($minPrice)
+    {
+        $this->minPrice = $minPrice;
+    }
+
+    /**
+     * Checks if Item price falls between bounds
+     * 
+     * @param Item $item
+     * 
+     * @return bool
+     */
+    public function isSatisfiedBy(Item $item)
+    {
+        if ( !empty($this->maxPrice) && $item->getPrice() > $this->maxPrice) {
+            return false;
+        }
+        if ( !empty($this->minPrice) && $item->getPrice() < $this->minPrice) {
+            return false;
+        }
+
+        return true;
+    }
+}
