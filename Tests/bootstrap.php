0dac8391f79803c4259d225f9f49f1953a84704e
Eloi Poch <eloi.poch@gmail.com>
2014-01-08 20:38:39 +0100

    run test suit using composer autoload
    Other improvements:
     - Move phpunit.xml to phpunit.xml.dist
     - Remove deprecated Tests/bootstrap.php

--- Tests/bootstrap.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-spl_autoload_register(function ($class) {
-    if (preg_match('#^DesignPatterns\\\\(.+)$#', $class, $ret)) {
-        $relPath = str_replace('\\', DIRECTORY_SEPARATOR, $ret[1]);
-        require_once dirname(__DIR__) . DIRECTORY_SEPARATOR . $relPath . '.php';
-    }
-});
