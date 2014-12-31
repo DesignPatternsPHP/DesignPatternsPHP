20c9d3f50924cbdc3c1a48f757fc7a1920e83d0d
Eddie Jaoude <eddie@jaoudestudios.com>
2014-12-31 23:18:51 +0000

    Updated config to use RTD theme

--- conf.py
+++ conf.py
@@ -256,3 +256,12 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# -- Customisation ------------------------------------------------
+
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
+if not on_rtd:  # only import and set the theme if we're building docs locally
+    import sphinx_rtd_theme
+    html_theme = 'sphinx_rtd_theme'
+    html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
