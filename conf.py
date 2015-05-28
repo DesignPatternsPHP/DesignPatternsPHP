48551e04fa8b2e53ecce245f06e2bed634dd4bd3
Faust <shangguokan@hotmail.com>
2015-05-28 19:00:45 +0200

    add Brazilian Portuguese, Spanish, Catalan translation initial files

--- conf.py
+++ conf.py
@@ -282,3 +282,7 @@ texinfo_documents = [
 
 # If true, do not generate a @detailmenu in the "Top" node's menu.
 #texinfo_no_detailmenu = False
+
+# -- Options for Translation -------------------------------------------
+locale_dirs = ['locale/']
+gettext_compact = False
