dff806b7c7c5a2ebb312e04fe29c9cc7c8caf591
Eddie Abou-Jaoude <eddie@jaoudestudios.com>
2015-08-16 09:16:38 +0100

    Singleton updated to PHP7 strict mode

--- Creational/Singleton/Singleton.php
+++ Creational/Singleton/Singleton.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_types = 1);
 
 namespace DesignPatterns\Creational\Singleton;
 
@@ -17,7 +18,7 @@ class Singleton
      *
      * @return self
      */
-    public static function getInstance()
+    public static function getInstance() : Singleton
     {
         if (null === static::$instance) {
             static::$instance = new static;
