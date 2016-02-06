3f1bd03aa46aa0192cb135571d2787fcfbf37f15
flobee <flobee@gmail.com>
2016-02-06 19:58:03 +0100

    Some more tests, the constant confuses a bit

--- Structural/Registry/Tests/RegistryTest.php
+++ Structural/Registry/Tests/RegistryTest.php
@@ -6,6 +6,19 @@ use DesignPatterns\Structural\Registry\Registry;
 
 class RegistryTest extends \PHPUnit_Framework_TestCase
 {
+    public function testSimpleGetSet()
+    {
+        $key = 'myIdentifier';
+        $object = new \StdClass();
+        $object->props = array('a' => 1, 'b' => 2);
+        
+        Registry::set($key, $object);
+        $actual = Registry::get($key);
+        
+        $this->assertEquals($object, $actual);
+        $this->assertInstanceOf('StdClass', $actual);
+    }
+    
     public function testSetAndGetLogger()
     {
         Registry::set(Registry::LOGGER, new \StdClass());
