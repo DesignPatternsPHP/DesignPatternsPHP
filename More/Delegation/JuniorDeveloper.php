2b1de13391b93179c640cf14e82838c41f004e7c
Dominik Liebler <liebler.dominik@gmail.com>
2016-09-22 15:28:46 +0200

    PHP7 Delegation

--- More/Delegation/JuniorDeveloper.php
+++ More/Delegation/JuniorDeveloper.php
@@ -4,7 +4,7 @@ namespace DesignPatterns\More\Delegation;
 
 class JuniorDeveloper
 {
-    public function writeBadCode()
+    public function writeBadCode(): string
     {
         return 'Some junior developer generated code...';
     }
