370ce4dd933b2af9c319c06fa495f7ce037947a5
Dominik Liebler <liebler.dominik@gmail.com>
2016-09-22 15:05:25 +0200

    PHP7 SimpleFactory

--- Creational/SimpleFactory/Tests/SimpleFactoryTest.php
+++ Creational/SimpleFactory/Tests/SimpleFactoryTest.php
@@ -4,40 +4,11 @@ namespace DesignPatterns\Creational\SimpleFactory\Tests;
 
 use DesignPatterns\Creational\SimpleFactory\SimpleFactory;
 
-/**
- * SimpleFactoryTest tests the Simple Factory pattern.
- */
 class SimpleFactoryTest extends \PHPUnit_Framework_TestCase
 {
-    protected $factory;
-
-    protected function setUp()
+    public function testCanCreateBicycle()
     {
-        $this->factory = new SimpleFactory();
-    }
-
-    public function getType()
-    {
-        return array(
-            array('bicycle'),
-            array('other'),
-        );
-    }
-
-    /**
-     * @dataProvider getType
-     */
-    public function testCreation($type)
-    {
-        $obj = $this->factory->createVehicle($type);
-        $this->assertInstanceOf('DesignPatterns\Creational\SimpleFactory\VehicleInterface', $obj);
-    }
-
-    /**
-     * @expectedException \InvalidArgumentException
-     */
-    public function testBadType()
-    {
-        $this->factory->createVehicle('car');
+        $bicycle = (new SimpleFactory())->createBicycle();
+        $this->assertInstanceOf('DesignPatterns\Creational\SimpleFactory\Bicycle', $bicycle);
     }
 }
