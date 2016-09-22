9b9eee5a4cefc1301df368b3215e2489e3c216a9
Dominik Liebler <liebler.dominik@gmail.com>
2016-09-22 10:54:22 +0200

    PHP7 Observer

--- Behavioral/Observer/UserObserver.php
+++ Behavioral/Observer/UserObserver.php
@@ -2,19 +2,28 @@
 
 namespace DesignPatterns\Behavioral\Observer;
 
-/**
- * class UserObserver.
- */
 class UserObserver implements \SplObserver
 {
     /**
-     * This is the only method to implement as an observer.
-     * It is called by the Subject (usually by SplSubject::notify() ).
+     * @var User[]
+     */
+    private $changedUsers = [];
+
+    /**
+     * It is called by the Subject, usually by SplSubject::notify()
      *
      * @param \SplSubject $subject
      */
     public function update(\SplSubject $subject)
     {
-        echo get_class($subject).' has been updated';
+        $this->changedUsers[] = clone $subject;
+    }
+
+    /**
+     * @return User[]
+     */
+    public function getChangedUsers(): array
+    {
+        return $this->changedUsers;
     }
 }
