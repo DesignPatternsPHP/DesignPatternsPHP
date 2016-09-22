19fff0aed9a3fe0f2b29ef699550459366b07bcb
Dominik Liebler <liebler.dominik@gmail.com>
2016-09-22 11:13:47 +0200

    PHP7 TemplateMethod

--- Behavioral/TemplateMethod/BeachJourney.php
+++ Behavioral/TemplateMethod/BeachJourney.php
@@ -2,16 +2,10 @@
 
 namespace DesignPatterns\Behavioral\TemplateMethod;
 
-/**
- * BeachJourney is vacation at the beach.
- */
 class BeachJourney extends Journey
 {
-    /**
-     * prints what to do to enjoy your vacation.
-     */
-    protected function enjoyVacation()
+    protected function enjoyVacation(): string
     {
-        echo "Swimming and sun-bathing\n";
+        return "Swimming and sun-bathing";
     }
 }
