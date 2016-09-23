e6c67c5da53e9bb7ab287ef13cc8c74fe26b9da7
Dominik Liebler <liebler.dominik@gmail.com>
2016-09-23 10:12:59 +0200

    PHP7 Dependency Injection

--- Structural/DependencyInjection/Tests/DependencyInjectionTest.php
+++ Structural/DependencyInjection/Tests/DependencyInjectionTest.php
@@ -2,24 +2,16 @@
 
 namespace DesignPatterns\Structural\DependencyInjection\Tests;
 
-use DesignPatterns\Structural\DependencyInjection\ArrayConfig;
-use DesignPatterns\Structural\DependencyInjection\Connection;
+use DesignPatterns\Structural\DependencyInjection\DatabaseConfiguration;
+use DesignPatterns\Structural\DependencyInjection\DatabaseConnection;
 
 class DependencyInjectionTest extends \PHPUnit_Framework_TestCase
 {
-    protected $config;
-    protected $source;
-
-    public function setUp()
-    {
-        $this->source = include 'config.php';
-        $this->config = new ArrayConfig($this->source);
-    }
-
     public function testDependencyInjection()
     {
-        $connection = new Connection($this->config);
-        $connection->connect();
-        $this->assertEquals($this->source['host'], $connection->getHost());
+        $config = new DatabaseConfiguration('localhost', 3306, 'domnikl', '1234');
+        $connection = new DatabaseConnection($config);
+
+        $this->assertEquals('domnikl:1234@localhost:3306', $connection->getDsn());
     }
 }
