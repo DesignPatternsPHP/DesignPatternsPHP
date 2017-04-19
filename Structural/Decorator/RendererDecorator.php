0da15dec7f46717dbf2749d93bb658adbb526b45
Eugene <danilenko.eugene@gmail.com>
2017-04-19 12:13:41 +0300

    Update RendererDecorator.php
    The Decorator MUST implement the RenderableInterface contract.

--- Structural/Decorator/RendererDecorator.php
+++ Structural/Decorator/RendererDecorator.php
@@ -3,11 +3,11 @@
 namespace DesignPatterns\Structural\Decorator;
 
 /**
- * the Decorator MUST implement the RendererInterface contract, this is the key-feature
+ * the Decorator MUST implement the RenderableInterface contract, this is the key-feature
  * of this design pattern. If not, this is no longer a Decorator but just a dumb
  * wrapper.
  */
-abstract class RendererDecorator
+abstract class RendererDecorator implements RenderableInterface
 {
     /**
      * @var RenderableInterface
