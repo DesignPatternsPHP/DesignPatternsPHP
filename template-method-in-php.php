9426febe965a109d3bcfc69e70a2be08f7eb81ed
Akshay22121995 <31428045+Akshay22121995@users.noreply.github.com>
2019-10-28 23:36:36 +0530

    Create template-method-in-php.php

---
+++ template-method-in-php.php
@@ -0,0 +1,119 @@
+<?php
+
+namespace RefactoringGuru\TemplateMethod\Conceptual;
+
+/**
+ * The Abstract Class defines a template method that contains a skeleton of some
+ * algorithm, composed of calls to (usually) abstract primitive operations.
+ *
+ * Concrete subclasses should implement these operations, but leave the template
+ * method itself intact.
+ */
+abstract class AbstractClass
+{
+    /**
+     * The template method defines the skeleton of an algorithm.
+     */
+    final public function templateMethod(): void
+    {
+        $this->baseOperation1();
+        $this->requiredOperations1();
+        $this->baseOperation2();
+        $this->hook1();
+        $this->requiredOperation2();
+        $this->baseOperation3();
+        $this->hook2();
+    }
+
+    /**
+     * These operations already have implementations.
+     */
+    protected function baseOperation1(): void
+    {
+        echo "AbstractClass says: I am doing the bulk of the work\n";
+    }
+
+    protected function baseOperation2(): void
+    {
+        echo "AbstractClass says: But I let subclasses override some operations\n";
+    }
+
+    protected function baseOperation3(): void
+    {
+        echo "AbstractClass says: But I am doing the bulk of the work anyway\n";
+    }
+
+    /**
+     * These operations have to be implemented in subclasses.
+     */
+    abstract protected function requiredOperations1(): void;
+
+    abstract protected function requiredOperation2(): void;
+
+    /**
+     * These are "hooks." Subclasses may override them, but it's not mandatory
+     * since the hooks already have default (but empty) implementation. Hooks
+     * provide additional extension points in some crucial places of the
+     * algorithm.
+     */
+    protected function hook1(): void { }
+
+    protected function hook2(): void { }
+}
+
+/**
+ * Concrete classes have to implement all abstract operations of the base class.
+ * They can also override some operations with a default implementation.
+ */
+class ConcreteClass1 extends AbstractClass
+{
+    protected function requiredOperations1(): void
+    {
+        echo "ConcreteClass1 says: Implemented Operation1\n";
+    }
+
+    protected function requiredOperation2(): void
+    {
+        echo "ConcreteClass1 says: Implemented Operation2\n";
+    }
+}
+
+/**
+ * Usually, concrete classes override only a fraction of base class' operations.
+ */
+class ConcreteClass2 extends AbstractClass
+{
+    protected function requiredOperations1(): void
+    {
+        echo "ConcreteClass2 says: Implemented Operation1\n";
+    }
+
+    protected function requiredOperation2(): void
+    {
+        echo "ConcreteClass2 says: Implemented Operation2\n";
+    }
+
+    protected function hook1(): void
+    {
+        echo "ConcreteClass2 says: Overridden Hook1\n";
+    }
+}
+
+/**
+ * The client code calls the template method to execute the algorithm. Client
+ * code does not have to know the concrete class of an object it works with, as
+ * long as it works with objects through the interface of their base class.
+ */
+function clientCode(AbstractClass $class)
+{
+    // ...
+    $class->templateMethod();
+    // ...
+}
+
+echo "Same client code can work with different subclasses:\n";
+clientCode(new ConcreteClass1);
+echo "\n";
+
+echo "Same client code can work with different subclasses:\n";
+clientCode(new ConcreteClass2);
