945171dcf3e6ec8ddd8b1920fe4109129979081a
Ashwin Kumar Ramaswamy <aaashwin515@gmail.com>
2020-10-23 18:53:27 +0530

    Added Factory PHP Design Pattern.

---
+++ Factory.php
@@ -0,0 +1,30 @@
+<?php
+class Automobile
+{
+    private $vehicleMake;
+    private $vehicleModel;
+
+    public function __construct($make, $model)
+    {
+        $this->vehicleMake = $make;
+        $this->vehicleModel = $model;
+    }
+
+    public function getMakeAndModel()
+    {
+        return $this->vehicleMake . ' ' . $this->vehicleModel;
+    }
+}
+
+class AutomobileFactory
+{
+    public static function create($make, $model)
+    {
+        return new Automobile($make, $model);
+    }
+}
+
+// have the factory create the Automobile object
+$veyron = AutomobileFactory::create('Bugatti', 'Veyron');
+
+print_r($veyron->getMakeAndModel()); // outputs "Bugatti Veyron"
