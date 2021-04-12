dea71e83b465b49721b39c411bee2f659e1de534
Roman Martinuk <romanmartinuk@gmail.com>
2021-04-13 00:29:24 +0300

    PHP 8

--- Behavioral/Interpreter/AndExp.php
+++ Behavioral/Interpreter/AndExp.php
@@ -4,17 +4,12 @@ namespace DesignPatterns\Behavioral\Interpreter;
 
 class AndExp extends AbstractExp
 {
-    private AbstractExp $first;
-    private AbstractExp $second;
-
-    public function __construct(AbstractExp $first, AbstractExp $second)
+    public function __construct(private AbstractExp $first, private AbstractExp $second)
     {
-        $this->first = $first;
-        $this->second = $second;
     }
 
     public function interpret(Context $context): bool
     {
-        return (bool) $this->first->interpret($context) && $this->second->interpret($context);
+        return $this->first->interpret($context) && $this->second->interpret($context);
     }
 }
--- Behavioral/Interpreter/OrExp.php
+++ Behavioral/Interpreter/OrExp.php
@@ -4,13 +4,8 @@ namespace DesignPatterns\Behavioral\Interpreter;
 
 class OrExp extends AbstractExp
 {
-    private AbstractExp $first;
-    private AbstractExp $second;
-
-    public function __construct(AbstractExp $first, AbstractExp $second)
+    public function __construct(private AbstractExp $first, private AbstractExp $second)
     {
-        $this->first = $first;
-        $this->second = $second;
     }
 
     public function interpret(Context $context): bool
