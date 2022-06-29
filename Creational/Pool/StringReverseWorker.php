33fe6d7ee6ebf3c6f895e420999d038d532786fc
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-06-30 00:53:41 +0300

    Removed unused constructor & Datetime

--- Creational/Pool/StringReverseWorker.php
+++ Creational/Pool/StringReverseWorker.php
@@ -4,14 +4,8 @@ declare(strict_types=1);
 
 namespace DesignPatterns\Creational\Pool;
 
-use DateTime;
-
 class StringReverseWorker
 {
-    public function __construct()
-    {
-    }
-
     public function run(string $text): string
     {
         return strrev($text);
