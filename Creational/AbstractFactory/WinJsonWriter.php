54f809298b8dfa428df3bca47bec4a281519f6dc
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-06-30 01:05:54 +0300

    WinJsonWriter :: missing formatted option added

--- Creational/AbstractFactory/WinJsonWriter.php
+++ Creational/AbstractFactory/WinJsonWriter.php
@@ -6,8 +6,12 @@ class WinJsonWriter implements JsonWriter
 {
     public function write(array $data, bool $formatted): string
     {
+        $options = 0;
 
+        if ($formatted) {
+            $options = JSON_PRETTY_PRINT;
+        }
 
-        return json_encode($data, JSON_PRETTY_PRINT);
+        return json_encode($data, $options);
     }
 }
