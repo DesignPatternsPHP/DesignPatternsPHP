719593cedcdfd355656dcfbafed96d99154dfe6e
Atakan Demircioğlu <31544037+atakde@users.noreply.github.com>
2022-07-19 09:01:15 +0300

    StaticFactory :: lint

--- Creational/StaticFactory/StaticFactory.php
+++ Creational/StaticFactory/StaticFactory.php
@@ -14,7 +14,7 @@ final class StaticFactory
 {
     public static function factory(string $type): Formatter
     {
-        return match($type) {
+        return match ($type) {
             'number' => new FormatNumber(),
             'string' => new FormatString(),
             default => throw new InvalidArgumentException('Unknown format given'),
